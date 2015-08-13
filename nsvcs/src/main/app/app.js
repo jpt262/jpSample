e57e8c579cb42d058874e227b53faadc86837b92
Jean-Paul Tres <jtres@ijento.com>
2015-08-13 10:44:08 -0400

    bringing in angular

--- nsvcs/src/main/app/app.js
+++ nsvcs/src/main/app/app.js
@@ -1,11 +1,9 @@
-var express = require('express');
+var express = require('express'),
+path = require('path');
 var app = express();
 
 app.use('/', express.static('public'));
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
+app.use("/bower", express.static(path.join(__dirname, '..', 'bower_components')));
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
